3061f60f560329aa1ce94a51e906eb40bca6fc31
Shirerpeton <xxxnekrosxxx@gmail.com>
2023-07-15 17:38:25 +0400

    init commit

---
+++ src/components/ToDoList.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import ToDo from '../components/ToDo.svelte'
+
+    interface ToDo {
+        id: number;
+        text: string;
+        done: boolean;
+    }
+
+    export let todos: ToDo[] = [];
+</script>
+
+<style>
+    .todos {
+        display: flex;
+        flex-direction: column;
+        width: 80%;
+    }
+</style>
+
+<div class="todos">
+    <h2>ToDo List:</h2>
+    {#each todos as todo}
+        <ToDo {...todo} />  
+    {/each}
+</div>
